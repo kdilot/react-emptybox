2f26daece21b4a1d776aedf02caa9d71973bfa69
kdilot <kdilot@gmail.com>
2018-09-13 21:04:16 -0700

    context

---
+++ client/src/context/Netflix.js
@@ -0,0 +1,3 @@
+import React from 'react';
+const Net = React.createContext(null);
+export default Net;
---
+++ client/src/context/Side.js
@@ -0,0 +1,3 @@
+import React from 'react';
+const Side = React.createContext(null);
+export default Side;
