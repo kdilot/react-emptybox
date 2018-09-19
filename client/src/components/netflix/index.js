2a2eaab5245b4452f262954a4212bd78bed82f23
kdilot <kdilot@gmail.com>
2018-09-18 20:20:51 -0700

    views

---
+++ client/src/components/netflix/index.js
@@ -0,0 +1,6 @@
+export {default as MovieSearch} from './MovieSearch'
+export {default as ItemList} from './ItemList'
+export {default as SideMenu} from './SideMenu'
+export {default as SliderButton} from './SliderButton'
+export {default as Profile} from './Profile'
+export {default as MovieInformation} from './MovieInformation'
