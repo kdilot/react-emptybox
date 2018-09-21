515ebe56f3a39fa6faecda5673eff894c74162e4
kdilot <kdilot@gmail.com>
2018-09-20 19:15:40 -0700

    views

---
+++ client/src/components/netflix/MovieInformation.js
@@ -0,0 +1,98 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Icon, Row, Col, Rate, Tag } from 'antd';
+import { Netflix } from 'context';
+import { Language } from 'common';
+import { CSSTransition } from 'react-transition-group';
+
+const CSS = styled.div`
+  .fade-enter {
+    opacity: 0.01;
+    transform: translateY(100%);
+  }
+  .fade-enter-active {
+    opacity: 1;
+    transform: translateY(0%);
+    -webkit-transition: all 300ms ease-out 0s;
+    transition: all 300ms ease-out 0s;
+  }
+  .fade-exit {
+    opacity: 1;
+    transform: translateY(0%);
+  }
+  .fade-exit-active {
+    opacity: 0.1;
+    transform: translateY(100%);
+    -webkit-transition: all 300ms ease-out 0s;
+    transition: all 300ms ease-out 0s;
+  }
+`
+const Info = styled.div`
+  width: 40vw;
+  height: 100vh;
+  margin-top: 2.5em;
+  overflow: auto;
+  background: #242424;
+  z-index: 2001;
+  display: flex;
+
+  img {
+    width: 100%;
+  }
+  h1, h2 {
+    font-size: 2.5em;
+    color: white;
+    padding: 0;
+    margin: 0 40px;
+  }
+  h2 {
+    margin-top: 0.3em;
+    font-size: 1.2em;
+    font-weight: normal;
+  }
+`
+
+const MovieInformation = () => {
+  return (
+    <Netflix.Consumer>
+      {net => {
+        return (
+          <CSS>
+            <CSSTransition
+              in={net.display.movieInfomation === 'block'}
+              classNames="fade"
+              timeout={300}
+              appear
+              mountOnEnter
+              unmountOnExit
+            >
+              <Row type="flex" justify="center">
+                <Info>
+                  <Row style={{ width: '100vw' }}>
+                    <Col span={24} style={{ textAlign: 'right' }}>
+                      <img alt="img" src={net.movieList.information.backdrop_path ? net.imgSize.large + net.movieList.information.backdrop_path : net.imgSize.medium + net.movieList.information.poster_path} />
+                      <Icon type="close" style={{ fontSize: 25, margin: '0.3em', position: 'absolute', right: 0, top: 0, fontWeight: 'bolder', color: 'black' }} onClick={() => { net.handleMovieModal(false) }} />
+                    </Col>
+                    <Col span={24}>
+                      <h1>{net.movieList.information ? net.movieList.information.title : ''}</h1>
+                      <h2>
+                        {net.movieList.information.genre_ids && net.movieList.information.genre_ids.length > 0 ? net.movieList.information.genre_ids.map(
+                          (list, index) => <Tag key={index}>{net.genres[list]}</Tag>
+                        ) : ''}
+                      </h2>
+                      <h2><Rate disabled value={net.movieList.information.vote_average} count={10} /></h2>
+                      <h2><b><Language value="ReleaseInfo" /> :</b> {net.movieList.information ? net.movieList.information.release_date : ''}</h2>
+                      <h2>{net.movieList.information ? net.movieList.information.overview : ''}</h2>
+                    </Col>
+                  </Row>
+                </Info>
+              </Row>
+            </CSSTransition>
+          </CSS>
+        )
+      }}
+    </Netflix.Consumer>
+  );
+};
+
+export default MovieInformation;
