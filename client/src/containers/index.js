615adac1be7d0918112f0c38cc50ee8592ad5e7b
kdilot <kdilot@gmail.com>
2018-09-23 20:58:00 -0700

    container

--- client/src/containers/index.js
+++ client/src/containers/index.js
@@ -1,2 +1,3 @@
 export { default as IncomeContainer } from './IncomeContainer';
 export { default as NetflixContainer } from './NetflixContainer'
+export { default as VtcContainer } from './VtcContainer'
