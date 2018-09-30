f8e37a4c8ea4c2ac87c6466cb7a39b6b52b61e26
kdilot <kdilot@gmail.com>
2018-09-29 18:11:32 -0700

    default

--- client/src/index.js
+++ client/src/index.js
@@ -12,7 +12,8 @@ import { rootSaga } from 'sagas';
 import logger from 'redux-logger';
 
 const sagaMiddleware = createSagaMiddleware()
-const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), applyMiddleware(sagaMiddleware, logger));
+// const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), applyMiddleware(sagaMiddleware))
+const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), applyMiddleware(sagaMiddleware, logger))
 sagaMiddleware.run(rootSaga)
 
 render((
