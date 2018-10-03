53bbb5bf26c7462aae585d3b4c6b3353e6f54077
kdilot <kdilot@gmail.com>
2018-10-02 19:34:44 -0700

    crawler

--- backend/src/crawler/index.js
+++ backend/src/crawler/index.js
@@ -2,63 +2,18 @@ require('dotenv').config();
 const Poloniex = require('lib/poloniex/');
 const CurrencyRate = require('db/model/CurrencyRate');
 const Socket = require('./socket');
+const Ws = require('./ws');
 const Db = require('db');
-const currencyPairMap = require('lib/poloniex/currencyPairMap');
 function _objectWithoutProperties(obj, keys) { var target = {}; for (var i in obj) { if (keys.indexOf(i) >= 0) continue; if (!Object.prototype.hasOwnProperty.call(obj, i)) continue; target[i] = obj[i]; } return target; }
+const func = require('./function');
 
 const init = async () => {
   await Db.connect()
-  await registCurrencyRate()
+  // await registCurrencyRate()
   Socket.connect()
+  Ws.connect()
   // importInitialChartData()
   // const pa = await CurrencyRate.showAll()
-}
-
-async function registCurrencyRate() {
-  const tickers = await Poloniex.getTickers()
-  console.log('[Drop]')
-  await CurrencyRate.drop()
-  const keys = Object.keys(tickers)
-  const param = keys.map(key => {
-    const ticker = tickers[key]
-    const data = Object.assign({ name: key }, ticker)
-    const currency = new CurrencyRate(data)
-    return currency.save()
-  })
-
-  try {
-    await Promise.all(param)
-  } catch (e) {
-    console.log(e)
-  }
-}
-
-async function importInitialChartData() {
-  const currencyPairs = []
-  for (let key in currencyPairMap) {
-    currencyPairs.push(currencyPairMap[key])
-  }
-  // console.log(currencyPairs)
-  const requests = Poloniex.getChardData(currencyPairs[0])
-  // await Promise.all(requests)
-  // console.log(requests)
-}
-
-async function updateCurrencyRate() {
-  const tickers = await Poloniex.getTickers()
-  const keys = Object.keys(tickers)
-  const param = keys.map(key => {
-    const ticker = tickers[key]
-    const convert = _objectWithoutProperties(ticker, 'id')
-    return CurrencyRate.updateTicker(key, convert)
-  })
-
-  try {
-    await Promise.all(param)
-  } catch (e) {
-    console.log(e)
-  }
-  console.log('[Updated Currency Rate]')
 }
 
 const messageChannel = {
@@ -70,7 +25,7 @@ const messageChannel = {
       try {
         await CurrencyRate.updateTicker(name, convert)
         // if (name === 'BTC_VI') {
-        //   console.log('[Update]', name, convert, new Date())
+          // console.log('[Update]', name, new Date())
         // }
       } catch (e) {
         console.log(e)
@@ -87,12 +42,20 @@ const messageConvert = (message, ws = false) => {
     messageChannel[channel](data)
 }
 
+Ws.handleMessage = (message) => {
+  messageConvert(message)
+}
+
 Socket.handleMessage = (message) => {
   messageConvert(message)
 }
 
 Socket.handleUpdate = () => {
-  updateCurrencyRate()
+  func.updateCurrencyRate()
+}
+
+Ws.handleUpdate = () => {
+  func.updateCurrencyRate()
 }
 
 
