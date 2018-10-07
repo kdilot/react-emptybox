390972a17a588b21618a963763882892334241bd
kdilot <kdilot@gmail.com>
2018-10-07 15:56:55 -0700

    virtual currency

---
+++ client/src/components/vtc/CurrencyList.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import { Row, Col, Icon } from 'antd';
+import { CurrencyCard, CurrencyType, CurrencyChart } from 'components/vtc';
+import { Language } from 'common';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  h1 { 
+    margin: 0;
+  }
+  text-align: center;
+`
+
+class CurrencyList extends Component {
+  render() {
+    const {
+      list,
+      currencyNameList,
+      handleCurrencyType,
+      selectedCurrencyType,
+    } = this.props
+    const type = list.map(_list => _list.name.split('_')[0]).reduce(function (a, b) {
+      if (a.indexOf(b) < 0) a.push(b);
+      return a;
+    }, []).sort((a, b) => {
+      if (a > b) return 1;
+      if (a < b) return -1;
+      return 0;
+    })
+    return (
+      <Row style={{ width: '96vw' }}>
+      <Col span={24}>
+        <CurrencyChart />
+      </Col>
+        {list.length ?
+          <Col span={24} style={{ textAlign: 'center' }}>
+            <CurrencyType
+              selected={selectedCurrencyType}
+              handleCurrencyType={handleCurrencyType}
+              type={type}
+            />
+          </Col>
+          :
+          ''}
+        {list.length ?
+          list.sort(
+            (a, b) => {
+              if (a.name > b.name) return 1;
+              if (a.name < b.name) return -1;
+              return 0;
+            }
+          ).filter(_list => _list.name.indexOf(selectedCurrencyType + '_') !== -1)
+            .map((_list, index) =>
+              <Col span={4} key={index}>
+                <CurrencyCard
+                  name={_list.name.split('_')}
+                  currency={currencyNameList.filter(e => e.currency === _list.name.split('_')[1]).map(info => info.name)}
+                  price={_list.last}
+                  volume={_list.baseVolume}
+                  change={_list.percentChange}
+                  date={_list.lastUpdate}
+                  selected={selectedCurrencyType}
+                />
+              </Col>
+            )
+          :
+          <Col span={24}>
+            <Wrapper>
+              <h1><Language value="UpdatingData" /></h1><Icon type="loading" theme="outlined" style={{ fontSize: '30px' }} />
+            </Wrapper>
+          </Col>
+        }
+      </Row>
+    );
+  }
+}
+
+export default CurrencyList;
---
+++ client/src/components/vtc/index.js
@@ -0,0 +1,4 @@
+export {default as CurrencyCard} from './CurrencyCard'
+export {default as CurrencyChart} from './CurrencyChart'
+export {default as CurrencyList} from './CurrencyList'
+export {default as CurrencyType} from './CurrencyType'
