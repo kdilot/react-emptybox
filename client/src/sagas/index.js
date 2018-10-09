e9b5278cde7142fa0edd2a50837d31de3fb17b6f
kdilot <kdilot@gmail.com>
2018-10-08 21:31:27 -0700

    sagas

--- client/src/sagas/index.js
+++ client/src/sagas/index.js
@@ -1,7 +1,7 @@
 import { all, call, put, takeLatest, fork } from 'redux-saga/effects';
 // import { delay } from 'redux-saga';
 import axios from 'axios';
-import { CURRENCY_STATE_SUC, CURRENCY_STATE } from 'modules/vtc';
+import { CURRENCY_STATE_SUC, CURRENCY_STATE, CURRENCY_NAME_SUC, CURRENCY_NAME } from 'modules/vtc';
 
 export function* currencyStateWatch() {
   yield takeLatest(CURRENCY_STATE, currencyState)
@@ -11,8 +11,18 @@ export function* currencyState() {
   yield put({ type: CURRENCY_STATE_SUC, payload: state.data })
 }
 
+export function* currencyNameWatch() {
+  yield takeLatest(CURRENCY_NAME, currencyName)
+}
+
+export function* currencyName() {
+  const state = yield call([axios, axios.get], '/api/vtc/currencyname')
+  yield put({ type: CURRENCY_NAME_SUC, payload: state.data })
+}
+
 export function* rootSaga() {
   yield all([
     fork(currencyStateWatch),
+    fork(currencyNameWatch),
   ])
 }
