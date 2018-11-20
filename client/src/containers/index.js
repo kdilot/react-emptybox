923d3452df3da73ced1f54a284b890359fcc04da
kdilot <kdilot@gmail.com>
2018-11-19 21:01:50 -0800

    container

--- client/src/containers/index.js
+++ client/src/containers/index.js
@@ -1,3 +1,4 @@
 export { default as NetflixContainer } from './NetflixContainer'
 export { default as VtcContainer } from './VtcContainer'
 export { default as ProductContainer } from './ProductContainer'
+export { default as DashboardContainer } from './DashboardContainer'
