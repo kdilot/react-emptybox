62ad20f369637748ba06d6ab9a62c1250fc08f6e
kdilot <kdilot@gmail.com>
2018-11-27 18:21:32 -0800

    dashboard header

--- client/src/components/dashboard/DashboardHeader.js
+++ client/src/components/dashboard/DashboardHeader.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import { Link } from 'react-router-dom';
 import { Language } from 'common';
 import { Col, Breadcrumb } from 'antd';
 import styled from 'styled-components';
@@ -19,9 +18,9 @@ class DashboardHeader extends Component {
       <BreadcrumbWrapper>
         <Breadcrumb>
           <Breadcrumb.Item >Dashboard</Breadcrumb.Item>
-          {pathname.filter((list, index) => index > 1).map((list, index) => {
+          {pathname.filter((_list, index) => index > 1).map((list, index) => {
             return (
-              <Breadcrumb.Item key={index}><Link to={list}><Language value={list.charAt(0).toUpperCase() + list.slice(1)} /></Link></Breadcrumb.Item>
+              <Breadcrumb.Item key={index}><Language value={list.charAt(0).toUpperCase() + list.slice(1)} /></Breadcrumb.Item>
             )
           })}
         </Breadcrumb>
