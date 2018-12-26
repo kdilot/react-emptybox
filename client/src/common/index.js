42c045ad8845b996f4317c203ba8fd2a49833ed0
kdilot <kdilot@gmail.com>
2018-12-25 17:42:13 -0800

    common

--- client/src/common/index.js
+++ client/src/common/index.js
@@ -1,5 +1,4 @@
 export {default as Language} from './Language'
-export {default as Theme} from './Theme'
 export {default as Top} from './Top'
 export {default as Modal} from './Modal'
 export {default as DrawerBox} from './DrawerBox'
