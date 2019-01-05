dada0bace3f4e764d1841ee18846892ccb699c60
kdilot <kdilot@gmail.com>
2019-01-05 14:12:52 -0800

    vtc

--- client/src/components/vtc/CurrencyCard.js
+++ client/src/components/vtc/CurrencyCard.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import { Row, Col } from 'antd';
-import styled from 'styled-components';
 import { Link } from 'react-router-dom';
-// import moment from 'moment';
+import { Language } from 'common';
+import styled from 'styled-components';
 
 const Wrapper = styled.div`
   margin: 0.7em;
@@ -32,6 +32,19 @@ const Wrapper = styled.div`
   .effect-minus {
     background:#C10202;
   }
+  .frozen {
+    background:#91bbbc;
+    opacity: 0.5;
+  }
+`
+const FrozenText = styled.div`
+  font-size: 2em;
+  font-weight: bold;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  color: red;
 `
 
 class CurrencyCard extends Component {
@@ -71,18 +84,19 @@ class CurrencyCard extends Component {
     clearTimeout(this.timeout)
   }
   render() {
-    const { name, currency, price, volume, change } = this.props
+    const { name, currency, price, volume, change, frozen } = this.props
     const { changed, effect } = this.state
     return (
       <Wrapper>
-        <Link to={`/vtc/${name}`}>
-          <Row style={{ padding: '1em' }} className={changed ? effect ? 'currencyCard effect-plue' : 'currencyCard effect-minus' : 'currencyCard'}>
+        <Link to={frozen ? `/vtc` : `/vtc/${name}`}>
+          <Row style={{ padding: '1em' }} className={frozen ? 'currencyCard frozen' : (changed ? effect ? 'currencyCard effect-plue' : 'currencyCard effect-minus' : 'currencyCard')}>
             <Col span={24} style={{ textAlign: 'right', fontWeight: 'bold' }}>{name.split('_')[1]}</Col>
             <Col span={24} style={{ textAlign: 'center' }}><h2>{parseFloat(price).toFixed(8)}</h2></Col>
             <Col span={24} style={{ textAlign: 'center' }} className={change > 0 ? 'plus' : (change === 0 ? '' : 'minus')}>({Math.round(parseFloat(change) * 10000) / 100}%)</Col>
             <Col span={24} style={{ textAlign: 'right' }}><h5>{currency}</h5></Col>
             <Col span={24} style={{ textAlign: 'left' }}><h5>Volume</h5> {Math.round(parseFloat(volume) * 1000) / 1000}</Col>
           </Row>
+          {frozen ? <FrozenText><Language text="Frozen" /></FrozenText> : ''}
         </Link>
       </Wrapper>
     );
